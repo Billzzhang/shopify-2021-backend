c5309cd60a3fc8cbff88b7a14d66ea7255c1349b
billzzhang <billzhang345@gmail.com>
2021-09-16 16:48:19 -0400

    destroy images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@
 
 # Controller to create, serve and destroy images
 class ImagesController < ApplicationController
-  before_action :set_image, only: %i[show]
+  before_action :set_image, only: %i[show destroy]
 
   def home
     redirect_to images_path
@@ -19,16 +19,32 @@ class ImagesController < ApplicationController
 
   # POST /images or /images.json
   def create
-    @image = Image.new(image_params)
+    successful_save = true
+    image_params[:files].each do |file|
+      @image = Image.new({file: file})
+      if !@image.save
+        successful_save = false
+        break
+      end
+    end
 
     respond_to do |format|
-      if @image.save
+      if successful_save
         format.html { redirect_to root_path, notice: 'Image was successfully created.' }
         format.json { render :show, status: :created, location: @image }
       else
-        format.html { render :index, status: :unprocessable_entity }
+        format.html { redirect_to root_path, notice: 'Image failed to be stored.' }
         format.json { render json: @image.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /images/1 or /images/1.json
+  def destroy
+    @image.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Image was successfully deleted." }
+      format.json { head :no_content }
     end
   end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 class Image < ApplicationRecord
-  has_many_attached :files
-  validates :files, presence:true
+  has_one_attached :file
+  validates :file, presence:true
   validate :correct_file_type
 
   private
 
   def correct_file_type
-    self.files.each do |file|
-      if file.content_type !~ (%r{^image/(jpeg|pjpeg|png|bmp)$})
-        errors.add(:file, "is not the right type")
-        return false
-      end
+    if self.file.content_type !~ (%r{^image/(jpeg|pjpeg|png|bmp)$})
+      errors.add(:file, "is not the right type")
+      return false
     end
   end
 end
